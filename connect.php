3b8959a9d7cbaccc7713f6307f3567770f758f64
zvrubliauskas <zvrubliauskas@gmail.com>
2016-11-20 20:32:50 -0600

    InventoryApp

---
+++ connect.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "us-cdbr-azure-central-a.cloudapp.net";
+$username = "bd731ce7cc045b";
+$password = "2c604e07";
+$dbname = "acsm_2dfbee11f8ab07b";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql= "SELECT Item_Name, Color, Quantity, Aisle_Location FROM inventory_table";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+       
+        echo "Name: " . $row["Item_Name"]. " " . $row["Color"]. "<br>";
+        echo "Email: " . $row["Quanity"]. "<br>";
+        echo "Comments: " . $row["Aisle_Location"]."<br><hr>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+
+?>
